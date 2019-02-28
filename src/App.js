34b297ac452456519bb3eae3b254c0392fc3e18d
Sushanth BC <sushanthbc@gmail.com>
2019-02-28 16:16:24 +0530

    eslint configuration with some fixes (#2)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component, } from 'react';
-import './App.css';
-import Person from './Person/Person'
-import DataList from './DataList'
-import data from './data'
+import React, { Component, } from "react";
+import "./App.css";
+import Person from "./Person/Person"
+import DataList from "./DataList"
+import data from "./data"
 
 
 class App extends Component {
